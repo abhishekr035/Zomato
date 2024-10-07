220be08334ad858f836a7b92cb232a73becacfca
abhishekr035 <abhishekrabhi35@gmail.com>
2024-10-07 15:36:16 +0530

    Zomato App Created

---
+++ Zomato.java
@@ -0,0 +1,10 @@
+public abstract class Zomato
+{
+
+     public void displayAllItems()
+     {
+         System.out.println("All Items Displayed...!");
+     }
+
+     public abstract void addItensToCart(int id);
+}
